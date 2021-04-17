3ce39514487187f62f7670f1070d53fb12c5b3ad
Kaan Hacıhaliloğlu <60143024+khdoex@users.noreply.github.com>
2021-04-18 00:15:55 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,22 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name="feateml",
+    author="khdoex,mrp3anut,kutayisgorur",
+   
+    
+    url="https://github.com/mrp3anut/featureml",
+    
+    packages=find_packages(),
+    install_requires=[
+   'numpy',
+   'scipy',
+   'pywt',
+   'obspy',
+	 'jupyter'], 
+
+    python_requires='>=3.6',
+)
 
