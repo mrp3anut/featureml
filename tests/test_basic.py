34f570bd87481c8ca08329417e6aa291571f661a
Kaan Hacıhaliloğlu <60143024+khdoex@users.noreply.github.com>
2021-04-18 00:46:13 +0300

    Update test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,1 +1,10 @@
+from feateml import feateml.core as ft
 
+def test_dim():
+    data = np.zeros((600,))
+    dt = 0.01
+    nf = 1
+    f_min = 1
+    f_max = 2
+    wl = 'morlet'
+    assert cwt(data = data, dt=dt,nf=nf,f_min=f_min,f_max=f_max,wl=wl).ndim == 2
