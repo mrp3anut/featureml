5a896b62ff5deb6ba6683d375e3fa3455d754163
Kaan Hacıhaliloğlu <60143024+khdoex@users.noreply.github.com>
2021-04-18 21:57:15 +0300

    Update core.py

--- feat/core.py
+++ feat/core.py
@@ -134,72 +134,148 @@ def parameter_calc(wl,dt,f_min,f_max,nf,w0):
     return params
 
 
-def print_wavelets():
+    
+def add_1ch(data, cwt_data):
     """"
-    Prints available wavelets
+    Adds cwt data to data with 1 channel
+    
+    Parameters
+    ----------
+    data : 1D array
+           data used in transform
+    
+    cwt_data : 2D array
+               continous wavelet transform of the data
+    
+    Returns
+    -------
+    2D array data = (1+nf, len(data))
     
     """
-    wavlist = ['morlet', 'ricker'] + pywt.wavelist(kind='continuous')
-    print(wavlist)
     
-def add_wl_ch(data,dt,f_min,f_max,nf,w0,wl):
+    if data.ndim == 1:
+        length = data.shape[0]
+    else:
+        length = data.shape[1]
+    ch = 1
+    nf = cwt_data.shape[0]
+    
+    ext_data = np.zeros((ch+nf, length))
+    ext_data[:ch, :] = data
+    ext_data[ch:, :] = cwt_data
+    return ext_data
+
+def multi_cwt(data,
+              dt,
+              nf=1,
+           f_min=1,
+           f_max=50,
+             wl ='morlet',
+             w0=5):
     """
-    Adds CWT applied data to raw data as extra channels. 
+    Continous Wavelet Transform with multichannel data 
     
     Parameters
     ------
-    data: 1D or 2D Array
-        data to transform and add
+    data: 2D Array  with shape (ch_number, len(data))
+        data to transform
     
     dt: float
         Sample distance in seconds
+    nf: int
+        Number of logarithmically spaced frequencies between fmin and fmax
     f_min: int, default=1
         Minimum frequency for transformation
     f_max: int , default=50
         Maximum frequency for transformation
-    nf: int
-        Number of logarithmically spaced frequencies between fmin and fmax
-    w0: int, default = 5
-        parameter for the wavelet, tradeoff between time and frequency resolution
     wl: str, default = morlet
         Wavelet to use, ex: morlet, ricker,
+    w0: int, default = 5
+        parameter for the wavelet, tradeoff between time and frequency resolution
     
+
+    Returns
+    -------
+    Continous wavelet transform applied data shape= (ch_number*nf, len(data))  
+    
+    
+    ch_number: number of channels that data contains
+    """
+    
+    ch_number= data.shape[0]
+    
+    length = data.shape[1]
+    
+    cwt_list = []
+    for ch in range(ch_number):
+        ch_data = data[ch]
+        cwt_op = cwt(ch_data,dt=dt,f_min=f_min,f_max=f_max,nf=nf,w0=w0,wl=wl)
+        cwt_list.append(cwt_op)
+    m_cwt = np.asarray(cwt_list)
+    m_cwt = np.resize(m_cwt, (ch_number*nf, length))
+    return m_cwt
+
+def add_multich(data,m_cwt):
+    """"
+    Adds cwt data to data with multi channel
+    
+    Parameters
+    ----------
+    data : 2D array
+           data used in transform
+    
+    cwt_data : 2D array
+               continous wavelet transform of the data
     
     Returns
     -------
-    Raw data extended with Continous wavelet transform applied data = (ch + ch*nf, len(data))
+    2D array data = (1+nf, len(data))
     """
-    
-    dim = data.ndim
-
-    if dim == 1:
-        
-        ch = 1
-        length = data.shape[0]
-        
-        cwt_op = cwt(data,dt=dt,f_min=f_min,f_max=f_max,nf=nf,w0=w0,wl=wl)
-        
-        ext_data = np.zeros((ch+nf, length))
-        ext_data[:ch, :] = data
-        ext_data[ch:, :] = cwt_op
-    
-    elif dim == 2:
-        
-        ch = data.shape[0]
-        length = data.shape[1]
-        
-        cwt_list = []
-        
-        for curr_ch in range(ch):
-            ch_data = data[curr_ch]
-            cwt_op = cwt(ch_data,dt=dt,f_min=f_min,f_max=f_max,nf=nf,w0=w0,wl=wl)
-            cwt_list.append(cwt_op)
-        
-        cwt_list = np.asarray(cwt_list)
-        cwt_list = np.resize(cwt_list, (ch*nf, length))
-        ext_data = np.concatenate((data, cwt_list), axis=0)
-             
+    ext_data = np.concatenate((data, m_cwt), axis=0)
     return ext_data
+
+def featurize_cwt(data,
+                  dt,
+                  nf=1,
+               f_min=1,
+               f_max=50,
+                 wl ='morlet',
+                 w0 = 5 ):
+    """
+    Applies CWT to data and then adds the result to the original data
+    
+    Parameters
+    ------
+    data: 1D or 2D Array
+        data to transform
+    
+    dt: float
+        Sample distance in seconds
+    nf: int
+        Number of logarithmically spaced frequencies between fmin and fmax
+    f_min: int, default=1
+        Minimum frequency for transformation
+    f_max: int , default=50
+        Maximum frequency for transformation
+    wl: str, default = morlet
+        Wavelet to use, ex: morlet, ricker,
+    w0: int, default = 5
+        parameter for the wavelet, tradeoff between time and frequency resolution
+    
+    Returns
+    -------
+    Extended data with CWT, if ndim = 1 : shape = (1+nf, len(data))
+    otherwise shape = (ch_number + ch_number*nf, len(data))  
+    """ 
+    if data.ndim == 1:
+        
+        cwt_data = cwt(data=data,dt=dt,f_min=f_min,f_max=f_max,nf=nf,w0=w0,wl=wl)
+        extended_data = add_1ch(data=data,cwt_data=cwt_data)
+    elif data.ndim == 2:
+        cwt_data = multi_cwt(data=data,dt=dt,f_min=f_min,f_max=f_max,nf=nf,w0=w0,wl=wl)
+        extended_data = add_multich(data=data, m_cwt=cwt_data)
+        
+    return extended_data
 
 
 
@@ -210,3 +286,12 @@ def plt_ch(ext_data):
     for i in range(ext_data.shape[0]):
         plt.plot(time, ext_data[i])
         plt.show()
+        
+ 
+def print_wavelets():
+    """"
+    Prints available wavelets
+    
+    """
+    wavlist = ['morlet', 'ricker'] + pywt.wavelist(kind='continuous')
+    print(wavlist)
