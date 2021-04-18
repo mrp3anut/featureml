38e77df0c740e5cbf927afe133f32bbdc2c2907a
Kaan Hacıhaliloğlu <60143024+khdoex@users.noreply.github.com>
2021-04-18 20:40:00 +0300

    Update core.py

--- feat/core.py
+++ feat/core.py
@@ -140,3 +140,72 @@ def print_wavelets():
     """
     wavlist = ['morlet', 'ricker'] + pywt.wavelist(kind='continuous')
     print(wavlist)
+    
+def add_wl_ch(data,dt,f_min,f_max,nf,w0,wl):
+    """
+    Adds CWT applied data to raw data as extra channels. 
+    
+    Parameters
+    ------
+    data: 1D or 2D Array
+        data to transform and add
+    
+    dt: float
+        Sample distance in seconds
+    f_min: int, default=1
+        Minimum frequency for transformation
+    f_max: int , default=50
+        Maximum frequency for transformation
+    nf: int
+        Number of logarithmically spaced frequencies between fmin and fmax
+    w0: int, default = 5
+        parameter for the wavelet, tradeoff between time and frequency resolution
+    wl: str, default = morlet
+        Wavelet to use, ex: morlet, ricker,
+    
+    
+    Returns
+    -------
+    Raw data extended with Continous wavelet transform applied data = (ch + ch*nf, len(data))
+    """
+    
+    dim = data.ndim
+
+    if dim == 1:
+        
+        ch = 1
+        length = data.shape[0]
+        
+        cwt_op = cwt(data,dt=dt,f_min=f_min,f_max=f_max,nf=nf,w0=w0,wl=wl)
+        
+        ext_data = np.zeros((ch+nf, length))
+        ext_data[:ch, :] = data
+        ext_data[ch:, :] = cwt_op
+    
+    elif dim == 2:
+        
+        ch = data.shape[0]
+        length = data.shape[1]
+        
+        cwt_list = []
+        
+        for curr_ch in range(ch):
+            ch_data = data[curr_ch]
+            cwt_op = cwt(ch_data,dt=dt,f_min=f_min,f_max=f_max,nf=nf,w0=w0,wl=wl)
+            cwt_list.append(cwt_op)
+        
+        cwt_list = np.asarray(cwt_list)
+        cwt_list = np.resize(cwt_list, (ch*nf, length))
+        ext_data = np.concatenate((data, cwt_list), axis=0)
+             
+    return ext_data
+
+
+
+def wavelet_plt(ext_data):
+    
+    time = np.arange(0, ext_data.shape[1])
+
+    for i in range(ext_data.shape[0]):
+        plt.plot(time, ext_data[i])
+        plt.show()
