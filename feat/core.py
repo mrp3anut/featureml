a46eb5a4f2cfbedb6f050b96febe3d17861fef55
Kaan Hacıhaliloğlu <60143024+khdoex@users.noreply.github.com>
2021-05-12 17:49:54 +0300

    Update core.py

--- feat/core.py
+++ feat/core.py
@@ -30,56 +30,19 @@ def featurize_cwt(data, dt, nf=1, f_min=1, f_max=50, wl='morlet',w0=5):
     
     Returns
     -------
-    Extended data with CWT, if ndim = 1 : shape = (1+nf, len(data))
-    otherwise shape = (ch_number + ch_number*nf, len(data))  
+    Extended data with CWT, with shape = (len(data, ch_number + ch_number*nf))  
     """ 
-    if data.ndim == 1:
-        
-        cwt_data = cwt(data=data, dt=dt, f_min=f_min, f_max=f_max, nf=nf, w0=w0, wl=wl)
-        extended_data = add_1ch(data=data,cwt_data=cwt_data)
-    elif data.ndim == 2:
-        cwt_data = multi_cwt(data=data, dt=dt, f_min=f_min, f_max=f_max, nf=nf, w0=w0, wl=wl)
-        extended_data = add_multich(data=data, m_cwt=cwt_data)
+
+    cwt_data = cwt(data=data, dt=dt, f_min=f_min, f_max=f_max, nf=nf, w0=w0, wl=wl)
+    extended_data = add_ch(data=data, m_cwt=cwt_data)
         
     return extended_data
     
     
 
-def cwt(data, dt, nf=1, f_min=1, f_max=50, wl='morlet', w0=5):
+def cwt(data, dt, nf=1, f_min=1, f_max=50, wl='morlet',w0=5):
     """
-    Continous Wavelet Transform
-    
-    Parameters
-    ------
-    data: 1D Array
-        data to transform
-    
-    dt: float
-        Sample distance in seconds
-    nf: int
-        Number of logarithmically spaced frequencies between fmin and fmax
-    f_min: int, default=1
-        Minimum frequency for transformation
-    f_max: int , default=50
-        Maximum frequency for transformation
-    wl: str, default = morlet
-        Wavelet to use, ex: 'morlet', 'ricker'
-    w0: int, default = 5
-        parameter for the wavelet, tradeoff between time and frequency resolution
-    
-    Returns
-    -------
-    Continous wavelet transform applied data shape= (nf, len(data))
-    """
-    cwt_ = wavelet_cwt(wl)
-    params = parameter_calc(wl=wl, dt=dt, f_min=f_min, f_max=f_max, nf=nf, w0=w0)
-    return cwt_(data, **params)
-    
-
-
-def multi_cwt(data, dt, nf=1, f_min=1, f_max=50, wl='morlet', w0=5):
-    """
-    Continous Wavelet Transform with multichannel data 
+    Continous Wavelet Transform with 1d and 2d data 
     
     Parameters
     ------
@@ -95,7 +58,7 @@ def multi_cwt(data, dt, nf=1, f_min=1, f_max=50, wl='morlet', w0=5):
     f_max: int , default=50
         Maximum frequency for transformation
     wl: str, default = morlet
-        Wavelet to use, ex: 'morlet', 'ricker'
+        Wavelet to use, ex: morlet, ricker,
     w0: int, default = 5
         parameter for the wavelet, tradeoff between time and frequency resolution
     
@@ -108,81 +71,60 @@ def multi_cwt(data, dt, nf=1, f_min=1, f_max=50, wl='morlet', w0=5):
     ch_number: number of channels that data contains
     """
     data = shaper(data)
-    ch_number= data.shape[1]
+    params = parameter_calc(wl=wl,dt=dt,f_min=f_min,f_max=f_max,nf=nf,w0=w0)
+    ch_number = data.shape[1] 
     length = data.shape[0]
+    cwt_ = wavelet_cwt(wl)
     
-    cwt_list = []
-    for ch in range(ch_number):
-        ch_data = data[:, ch]
-        cwt_op = cwt(ch_data, dt=dt, f_min=f_min, f_max=f_max, nf=nf, w0=w0, wl=wl)
-        cwt_list.append(cwt_op)
-        
-    cwt_ = np.asarray(cwt_list)
-    cwt_ = np.reshape(cwt_, (length, ch_number*nf))
-    return cwt_
+   
+    cwts = np.vstack([cwt_(data[:,ch],**params) for ch in range(ch_number)])  
+   
+    return cwts.T
 
 
-
-
-def add_multich(data, m_cwt):
-    """
+def add_ch(data,m_cwt):
+    """"
     Adds cwt data to data with multi channel
-    
     Parameters
     ----------
     data : 2D array
            data used in transform
-    
     cwt_data : 2D array
                continous wavelet transform of the data
-    
     Returns
     -------
-    2D array data = (1+nf, len(data))
+    2D array data = (len(data), ch*(1+nf))
     """
-    ext_data = np.concatenate((data, m_cwt), axis=0)
+    data = shaper(data)
+    ext_data = np.concatenate((data, m_cwt), axis=1)
     return ext_data
 
 
 
-def add_1ch(data, cwt_data):
+
+
+def shaper(data):
     """
-    Adds cwt data to data with 1 channel
+    arranges the shape of the input data into the form of (len(data), ch_number)
+    if data is 1d then output is (len(data), 1))
+
     
     Parameters
     ----------
-    data : 1D array
-           data used in transform
+    data : 1D or 2D data 
     
-    cwt_data : 2D array
-               continous wavelet transform of the data
     
     Returns
     -------
-    2D array data = (1+nf, len(data))
+    2D array , in the shape of (len(data), ch_number)
     
     """
-    
-    if data.ndim == 1:
-        length = data.shape[0]
-    else:
-        length = data.shape[1]
-    ch = 1
-    nf = cwt_data.shape[0]
-    
-    ext_data = np.zeros((ch+nf, length))
-    ext_data[:ch, :] = data
-    ext_data[ch:, :] = cwt_data
-    return ext_data
-
-def shaper(data):
-
     data = np.asarray(data)
     if data.ndim == 1:
         data = np.reshape(data, (len(data), 1))
     elif data.ndim == 2:
         if data.shape[0] < data.shape[1]:
-            data = np.reshape(data, (data.shape[1], data.shape[0]))
+            data = data.T
     else:
         raise Exception("This function only works with 1d and 2d data")
     return data
