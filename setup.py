40b7ddc6ec61e422a6d39f96d9cbe71a43db12f0
Sinan Parmar <sinanparmar2@gmail.com>
2021-05-26 22:44:28 +0300

    updated requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -7,15 +7,14 @@ setup(
     author="khdoex,mrp3anut,EnesKutay",
    
     
-    url="https://github.com/boun-earth-ml/earth-ml/featureml",
+    url="https://github.com/mrp3anut/featureml",
     
     packages=find_packages(),
+    setup_requires=['numpy'],
     install_requires=[
-   'numpy',
    'scipy',
    'PyWavelets',
-   'obspy',
-   'jupyter'], 
+   'obspy'], 
 
    )
 
