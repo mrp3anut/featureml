09f199b123fd6f068fcb4941abb7cb62fe6374be
Kaan Hacıhaliloğlu <60143024+khdoex@users.noreply.github.com>
2021-06-06 14:59:29 +0300

    Update test_featurize_cwt.py

--- tests/test_featurize_cwt.py
+++ tests/test_featurize_cwt.py
@@ -6,7 +6,7 @@ from feat.core import cwt, featurize_cwt
 
 
 F_MIN = 1
-F_MAX = 50
+F_MAX = 10
 W0 = 5
 
 
@@ -19,12 +19,11 @@ def test_cwt_shape_morl():
     	assert cwt(data=data, dt=dt, nf=nf, f_min=F_MIN, f_max=F_MAX, wl=wl, w0=W0).shape == (600,2) 
     	
 def test_cwt_shape_mexh():
-    data = np.zeros((600,))
-    dt = 0.01
-    nf = 2
-    wavelets = ['mexh', 'ricker']
-    for wl in wavelets:
-    	assert cwt(data=data, dt=dt, nf=nf, f_min=F_MIN, f_max=F_MAX, wl=wl, w0=None).shape == (600,2) 
+    data = np.ones((600,))
+    widths = np.arange(1,31)
+    dt=0.01
+    wl='ricker'
+    assert cwt(data=data,dt=dt, widths=widths, wl=wl).shape == (600,30) 
     	    	
     
 def test_featurize_cwt_shape():
@@ -55,7 +54,7 @@ def test_complex_linearity(tol=1e-7):
     assert_equal(cfs.real.dtype, sst.dtype)  
 
     sst_complex = sst + 1j*sst                 # We create a new complex data
-    cfs_complex = cwt(sst_complex, wl=wl, f_min=F_MIN, f_max=F_MAX, dt=dt) # Then we take cwt again on complex data 
+    cfs_complex = cwt(sst_complex, dt=dt, nf=nf, f_min=F_MIN, f_max=F_MAX, wl=wl, w0=W0) # Then we take cwt again on complex data 
     
     
     assert_allclose(cfs + 1j*cfs, cfs_complex, atol=tol, rtol=tol) # Complex valued transform equals to sum of 
@@ -78,4 +77,4 @@ def test_featurize_extend(tol=1e-7):
     extended = featurize_cwt(data=data, dt=dt, nf=nf, f_min=F_MIN, f_max=F_MAX, wl=wl,w0=W0)
     cwts = cwt(data=data, dt=dt, nf=nf, f_min=F_MIN, f_max=F_MAX, wl=wl,w0=W0)
     assert_allclose(sst, extended[:,0], atol=tol, rtol=tol) # Check that first channel of the extended data equals to original data
-    assert_allclose(cwts, extended[:,1], atol=tol, rtol=tol)
+    assert_allclose(cwts[:,0], extended[:,1], atol=tol, rtol=tol)
